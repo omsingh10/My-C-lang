36a16186b29128a184a7f328a901e7f12a948264
Om singh(shiv) <114778480+omsingh10@users.noreply.github.com>
2024-01-27 21:46:47 +0530

    Add files via upload

---
+++ L1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+   
+   void main(){
+
+      int n,x;
+      printf("enter a number\n");
+      scanf("%d%d",&n,&x);
+       printf("enter a number is : %d" , n+x );
+      
+
+
+
+   }
+
+
+
+
