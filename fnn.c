d1089c1dd27fe5220042b3406ba34206a200c94b
Om singh(shiv) <114778480+omsingh10@users.noreply.github.com>
2024-01-30 03:09:36 +0530

    Add files via upload
    for funtion

---
+++ fnn.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ int a,b;
+
+ int sum(){
+    printf("\nenter 2 num"); 
+scanf("%d%d", &a ,&b);
+
+   int c = a + b ;
+   printf(" sum is %d", c);
+   return 0;
+ }
+
+void main(){
+ 
+
+sum();
+
+
+
+}
+
+
+
